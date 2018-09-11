e9330c99538a01b25b19f1558541cf103532d96e
hb240 <15629075991@163.com>
2018-09-11 21:50:24 +0800

    First commit

---
+++ homework/getproduct1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace getproduct1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("请输入两个数：");
+            int num1 = Convert.ToInt32(Console.ReadLine());
+            int num2 = Convert.ToInt32(Console.ReadLine());
+            Console.Write("乘积为：");
+            Console.WriteLine(num1 * num2);                //输出乘积
+            Console.ReadLine();                            //暂停
+        }
+    }
+}
---
+++ homework/getproduct2/Form1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace getproduct2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        //给按钮附加事件：将前面两个textbox的内容相乘结果填到label3中
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int text1 = int.Parse(textBox1.Text);             //类型转换
+            int text2 = int.Parse(textBox2.Text);
+            int product = text1 * text2;
+            label2.Text = product.ToString();
+        }
+    }
+}
